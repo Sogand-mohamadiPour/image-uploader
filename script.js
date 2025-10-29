1913537e2c890ced6174dac804ab331fc0618a86
Sogand <sogandmohamadipour7@gmail.com>
2025-10-29 23:29:42 +0330

    part 2 of challenge

--- script.js
+++ script.js
@@ -1,17 +1,58 @@
 'use strict';
 
-const imgPath = 'img/img-1.jpg';
+const wait = function (seconds) {
+    return new Promise(function (resolve) {
+        setTimeout(resolve, seconds * 1000);
+    });
+};
 
-function CreateImage(imgPath) {
+const imgContainer = document.querySelector('.images');
+
+const CreateImage = function (imgPath) {
     return new Promise(function (resolve, reject) {
         const img = document.createElement('img');
         img.src = imgPath;
+
         img.addEventListener('load', function () {
-            document.body.appendChild(img);
+            imgContainer.appendChild(img);
             resolve(img);
         });
+
         img.addEventListener('error', function () {
             reject(new Error('Image not found'));
         });
     });
-}
+};
+
+let currentImg;
+
+CreateImage('img/img-1.jpg')
+    .then(img => {
+        currentImg = img;
+        console.log('Image 1 loaded');
+        return wait(2)
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return CreateImage('img/img-2.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 2 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+        return CreateImage('img/img-3.jpg');
+    })
+    .then(img => {
+        currentImg = img;
+        console.log('Image 3 loaded');
+        return wait(2);
+    })
+    .then(() => {
+        currentImg.style.display = 'none';
+    })
+    .catch(err => {
+        console.error(err);
+    });
