5ad40107c38a7b2503833d3b5d13931452152038
Sogand <sogandmohamadipour7@gmail.com>
2025-11-01 21:41:48 +0330

    part 2: changing function to async await

--- script.js
+++ script.js
@@ -25,34 +25,60 @@ const CreateImage = function (imgPath) {
 };
 
 let currentImg;
+// part 1
 
-CreateImage('img/img-1.jpg')
-    .then(img => {
-        currentImg = img;
+// CreateImage('img/img-1.jpg')
+//     .then(img => {
+//         currentImg = img;
+//         console.log('Image 1 loaded');
+//         return wait(2)
+//     })
+//     .then(() => {
+//         currentImg.style.display = 'none';
+//         return CreateImage('img/img-2.jpg');
+//     })
+//     .then(img => {
+//         currentImg = img;
+//         console.log('Image 2 loaded');
+//         return wait(2);
+//     })
+//     .then(() => {
+//         currentImg.style.display = 'none';
+//         return CreateImage('img/img-3.jpg');
+//     })
+//     .then(img => {
+//         currentImg = img;
+//         console.log('Image 3 loaded');
+//         return wait(2);
+//     })
+//     .then(() => {
+//         currentImg.style.display = 'none';
+//     })
+//     .catch(err => {
+//         console.error(err);
+//     });
+
+// part2
+const loadNpause = async function() {
+    try {
+        // load image 1
+        let img = await CreateImage('img/img-1.jpg');
         console.log('Image 1 loaded');
-        return wait(2)
-    })
-    .then(() => {
-        currentImg.style.display = 'none';
-        return CreateImage('img/img-2.jpg');
-    })
-    .then(img => {
-        currentImg = img;
+        await wait(2);
+        img.style.display = 'none';
+        // load image 2
+        img = await CreateImage('img/img-2.jpg');
         console.log('Image 2 loaded');
-        return wait(2);
-    })
-    .then(() => {
-        currentImg.style.display = 'none';
-        return CreateImage('img/img-3.jpg');
-    })
-    .then(img => {
-        currentImg = img;
+        await wait(2);
+        img.style.display = 'none';
+        // load image 3
+        img = await CreateImage('img/img-3.jpg');
         console.log('Image 3 loaded');
-        return wait(2);
-    })
-    .then(() => {
-        currentImg.style.display = 'none';
-    })
-    .catch(err => {
+        await wait(2);
+        img.style.display = 'none';
+    }
+    catch (err) {
         console.error(err);
-    });
+    }
+}
+loadNpause();
